267e9ba8f9e506f6153d0e90b181450041d505c4
Yago Riccomi <yago.riccomi.s@gmail.com>
2021-12-03 12:19:22 -0300

    Navbar add

---
+++ loja/navbar.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<!-- Designined by CodingLab | www.youtube.com/codinglabyt -->
+<html lang="pt-br" dir="ltr">
+
+<head>
+  <meta charset="UTF-8">
+  <title> Página Principal </title>
+  <link rel="stylesheet" href="style.css">
+  <!-- Boxiocns CDN Link -->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+  <div class="sidebar close">
+    <div class="logo-details">
+      <i class="bi bi-globe"></i>
+      <span class="logo_name">Loja</span>
+    </div>
+    <ul class="nav-links">
+      <li>
+        <a href="#">
+          <i class="bi bi-shop"></i>
+          <span class="link_name">Vender</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Vender</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class="bi bi-pen"></i>
+            <span class="link_name">Cadastrar</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Cadastrar</a></li>
+          <li><a href="#">Cliente</a></li>
+          <li><a href="#">Produto</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class="bi bi-search"></i>
+            <span class="link_name">Pesquisar</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Pesquisar</a></li>
+          <li><a href="#">Cliente</a></li>
+          <li><a href="#">Produto</a></li>
+        </ul>
+      </li>
+      <li>
+        <div class="iocn-link">
+          <a href="#">
+            <i class="bi bi-calculator-fill"></i>
+            <span class="link_name">Registros</span>
+          </a>
+          <i class='bx bxs-chevron-down arrow'></i>
+        </div>
+        <ul class="sub-menu">
+          <li><a class="link_name" href="#">Registros</a></li>
+          <li><a href="#">Vendas</a></li>
+          <li><a href="#">Gastos</a></li>
+          <li><a href="#">Lucros</a></li>
+          <li><a href="#">Geral</a></li>
+        </ul>
+      </li>
+      <li>
+        <a href="#">
+          <i class="bi bi-door-open-fill"></i>
+          <span class="link_name">Sair</span>
+        </a>
+        <ul class="sub-menu blank">
+          <li><a class="link_name" href="#">Sair</a></li>
+        </ul>
+      </li>
+    </ul>
+  </div>
+  <section class="home-section">
+    <div class="home-content">
+      <i class='bx bi-list'></i>
+    </div>
+  </section>
+  <script src="script.js"></script>
+
+</body>
+
+</html>
