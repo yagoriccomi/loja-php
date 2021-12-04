39863fe2e3417b3ca18a3564dc663771a2dcd843
Yago Riccomi <yago.riccomi.s@gmail.com>
2021-12-03 21:59:00 -0300

    Organização

--- loja/cadastro.php
+++ loja/cadastro.php
@@ -6,11 +6,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="Content-Language" content="pt-br, en" />
-    <title>Login</title>
+    <title>Cadastro</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="css/global.css" />
     <link rel="stylesheet" href="css/navbar.css">
-    <link rel="stylesheet" href="css/menu.css">
+    <link rel="stylesheet" href="css/cadastro.css">
 </head>
 
 <body>
